928416cbc3d5905f195d4f36fa2ed49643e0e0f6
Tobia De Koninck <LEDfan@users.noreply.github.com>
2019-07-31 10:08:48 +0200

    Handle OneWays in OsmWayProcessor

--- src/main/kotlin/be/ledfan/geocoder/importer/processors/OsmWayProcessor.kt
+++ src/main/kotlin/be/ledfan/geocoder/importer/processors/OsmWayProcessor.kt
@@ -1,17 +1,15 @@
 package be.ledfan.geocoder.importer.processors
 
-import be.ledfan.geocoder.db.entity.OneWayRestriction
-import be.ledfan.geocoder.importer.core.BaseProcessor
-//import be.ledfan.geocoder.db.entity.OneWayRestriction
 import be.ledfan.geocoder.db.mapper.*
 import be.ledfan.geocoder.importer.DetermineLayerWay
 import be.ledfan.geocoder.importer.Layer
+import be.ledfan.geocoder.importer.core.BaseProcessor
 import be.ledfan.geocoder.importer.core.TagParser
+import be.ledfan.geocoder.importer.core.Tags
 import be.ledfan.geocoder.importer.core.copyVersionAndTags
-import be.ledfan.geocoder.db.entity.OsmWay as dbOsmWay
 import de.topobyte.osm4j.core.model.iface.OsmWay
 import mu.KotlinLogging
-import java.sql.Connection
+import be.ledfan.geocoder.db.entity.OsmWay as dbOsmWay
 
 /**
  * Process OsmWay objects.
@@ -20,11 +18,8 @@ class OsmWayProcessor(private val osmUpstreamLineMapper: OsmUpstreamLineMapper,
                       private val osmUpstreamPolygonMapper: OsmUpstreamPolygonMapper,
                       private val osmWayMapper: OsmWayMapper,
                       private val wayNodeMapper: WayNodeMapper,
-                      private val oneWayRestrictionMapper: OneWayRestrictionMapper,
                       private val tagParser: TagParser,
                       private val determineLayer: DetermineLayerWay) : BaseProcessor<OsmWay>() {
-
-    private val oneWayRestrictionObjects = ArrayList<OneWayRestriction>()
 
     private val logger = KotlinLogging.logger {}
 
@@ -71,7 +66,7 @@ class OsmWayProcessor(private val osmUpstreamLineMapper: OsmUpstreamLineMapper,
                 0 -> logger.trace { "Skipping ${dbObject.id} because no suitable layer found" }
                 1 -> {
                     dbObject.assignLayer(layers.first())
-//                    checkAndProcessOneWay(dbObject, tags) TODO
+                    checkAndProcessOneWay(dbObject, tags)
                     dbObjects.add(dbObject)
                     if (determineLayer.importNodesForLayer(layers.first())) {
                         for (idx in 0 until nodeIds.size) {
@@ -85,24 +80,25 @@ class OsmWayProcessor(private val osmUpstreamLineMapper: OsmUpstreamLineMapper,
 
         osmWayMapper.bulkInsert(dbObjects)
         wayNodeMapper.bulkInsert(wayNodes)
-//        oneWayRestrictionMapper.bulkInsert(oneWayRestrictionObjects)
-//        oneWayRestrictionObjects.clear()
     }
 
-//    private fun checkAndProcessOneWay(dbObject: be.ledfan.geocoder.db.entity.OsmWay, tags: Tags) {
-//        dbObject.hasOneWayRestriction = when {
-//            (tags.hasChild("highway") && tags.getSingleValueOfChild("highway") == "motorway") -> true
-//            (tags.hasChild("junction") && tags.getSingleValueOfChild("junction") == "roundabout") -> true
-//            (tags.hasChild("junction") && tags.getSingleValueOfChild("junction") == "circular") -> true
-//            (tags.hasChild("oneway") && (tags.getSingleValueOfChild("oneway") == "yes" || tags.getSingleValueOfChild("oneway") == "1" || tags.getSingleValueOfChild("oneway") == "true")) -> true
-//            else -> false;
-//        }
-//
-//        if (!dbObject.hasOneWayRestriction) {
-//            return
-//        }
-//
-//        dbObject.hasReversedOneWay = tags.hasChild("oneway") && (tags.getSingleValueOfChild("oneway") == "-1" || tags.getSingleValueOfChild("oneway") == "reverse")
-//    }
+    private fun checkAndProcessOneWay(dbObject: be.ledfan.geocoder.db.entity.OsmWay, tags: Tags) {
+        dbObject.hasOneWayRestriction = tags.childOrNull("highway")?.singleValueOrNull() == "motorway" ||
+                tags.childOrNull("junction")?.singleValueOrNull() == "roundabout" ||
+                tags.childOrNull("junction")?.singleValueOrNull() == "circular" ||
+                tags.childOrNull("oneway")?.singleValueOrNull() == "yes" ||
+                tags.childOrNull("oneway")?.singleValueOrNull() == "true" ||
+                tags.childOrNull("oneway")?.singleValueOrNull() == "reverse" ||
+                tags.childOrNull("oneway")?.singleValueOrNull() == "1" ||
+                tags.childOrNull("oneway")?.singleValueOrNull() == "-1"
+
+        if (!dbObject.hasOneWayRestriction) {
+            return
+        }
+
+        dbObject.hasReversedOneWay = tags.childOrNull("oneway")?.singleValueOrNull() == "reverse"
+                || tags.childOrNull("oneway")?.singleValueOrNull() == "-1"
+
+    }
 
 }
