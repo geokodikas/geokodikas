7db429f099ccb61ade53d11e07bee29ff0521eef
Tobia De Koninck <LEDfan@users.noreply.github.com>
2019-07-31 10:21:53 +0200

    Disable one_way_restriction table

--- src/main/kotlin/be/ledfan/geocoder/importer/steps/Step1.kt
+++ src/main/kotlin/be/ledfan/geocoder/importer/steps/Step1.kt
@@ -84,8 +84,8 @@ suspend fun step1_create_indexes() : Boolean {
         "CREATE UNIQUE INDEX IF NOT EXISTS osm_way_osm_id_index ON osm_way (osm_id)",
         "CREATE INDEX IF NOT EXISTS way_node_node_id_index ON way_node (node_id)",
         "CREATE INDEX IF NOT EXISTS way_node_way_id_index ON way_node (way_id)",
-        "CREATE INDEX IF NOT EXISTS osm_way_centroid_index ON osm_way USING GIST(centroid)",
-        "CREATE INDEX IF NOT EXISTS one_way_restrictions_way_id_from_node_id_index ON one_way_restrictions (way_id, from_node_id)")
+        "CREATE INDEX IF NOT EXISTS osm_way_centroid_index ON osm_way USING GIST(centroid)")
+//        "CREATE INDEX IF NOT EXISTS one_way_restrictions_way_id_from_node_id_index ON one_way_restrictions (way_id, from_node_id)")
 
     return executeBatchQueries(sqlQueries)
 }
