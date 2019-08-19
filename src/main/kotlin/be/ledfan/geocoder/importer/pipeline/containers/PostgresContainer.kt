b73d336711d1315121a2712a478179a5fa1cc7da
Tobia De Koninck <LEDfan@users.noreply.github.com>
2019-08-19 16:59:16 +0200

    Fix waiting and timeouts in postgres container

--- src/main/kotlin/be/ledfan/geocoder/importer/pipeline/containers/PostgresContainer.kt
+++ src/main/kotlin/be/ledfan/geocoder/importer/pipeline/containers/PostgresContainer.kt
@@ -15,7 +15,7 @@ class KPostgreSQLContainer(dockerImageName: String) : PostgreSQLContainer<KPostg
                 .withRegEx(".*database system is ready to accept connections.*\\s")
                 .withTimes(1) // Very important that this is 1 instead of 2 for containers which already
                 // contain a postgresql data directory, either as volume or with a committed container
-                .withStartupTimeout(Duration.of(60, SECONDS))
+                .withStartupTimeout(Duration.of(180, SECONDS))
 
         withDatabaseName("geokodikas")
         withUsername("geokodikas")
@@ -46,9 +46,6 @@ fun setupPostgresContainer(existingImage: String): KPostgreSQLContainer {
 
     val waitingConsumer = WaitingConsumer()
     postgresContainer.followOutput(waitingConsumer, OutputFrame.OutputType.STDERR)
-    waitingConsumer.waitUntil({ frame ->
-        frame.utf8String.contains("database system is ready to accept connections")
-    }, 360, TimeUnit.SECONDS, 1)
 
     return postgresContainer
 }
