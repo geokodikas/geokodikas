b6244c684acfb985fd666006180001e781f4b84e
Tobia De Koninck <LEDfan@users.noreply.github.com>
2019-08-22 10:52:09 +0200

    Add extra required potsgres extension

--- docker/postgis/initdb-postgis.sh
+++ docker/postgis/initdb-postgis.sh
@@ -17,5 +17,6 @@ for DB in template_postgis "$POSTGRES_DB"; do
 	"${psql[@]}" --dbname="$DB" <<-'EOSQL'
 		CREATE EXTENSION IF NOT EXISTS postgis;
 		CREATE EXTENSION IF NOT EXISTS hstore;
+                CREATE EXTENSION btree_gist;
 EOSQL
 done
