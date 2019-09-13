8b817e2e3e076b54e01e8f5aebde73acf3201250
Tobia De Koninck <LEDfan@users.noreply.github.com>
2019-09-13 11:39:05 +0200

    Cleanup old docker config

--- docker/osm2pgsql/create_users.sh
+++
@@ -1,8 +0,0 @@
-if [ "$(getent group "${HOST_GID}")" ]; then
-    echo "Group with GID: ${HOST_GID} already exists"
-else
-    echo "Creating group with GID: ${HOST_GID} and name users"
-    groupadd -g "${HOST_GID}" users
-fi
-echo "Creating user with GID: ${HOST_GID} and UID: ${HOST_UID} and name 'luser'"
-useradd -u "${HOST_UID}" --gid "${HOST_GID}" luser
